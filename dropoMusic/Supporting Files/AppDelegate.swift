c36d3c8d949e46a1e65cc1c6005cb412c348d52d
Anton Lisitsyn <lisitsyn.anton.a@gmail.com>
2017-12-12 11:59:10 +0300

    added DropBoxSettings struct, modified AppDelegate

--- dropoMusic/Supporting Files/AppDelegate.swift
+++ dropoMusic/Supporting Files/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import SwiftyDropbox
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        DropboxClientsManager.setupWithAppKey(DropBoxSettings.DROPBOX_APP_KEY)
         // Override point for customization after application launch.
         return true
     }
---
+++ dropoMusic/Supporting Files/DropBoxSettings.swift
@@ -0,0 +1,12 @@
+//
+//  DropBoxSettings.swift
+//  dropoMusic
+//
+//  Created by Anton on 12.12.17.
+//  Copyright © 2017 Anton. All rights reserved.
+//
+
+import Foundation
+struct DropBoxSettings {
+    static let DROPBOX_APP_KEY: String = "xvuh5rst0bsm4e2"
+}
