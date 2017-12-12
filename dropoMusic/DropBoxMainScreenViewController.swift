f9d97aa111ffbfb0bca5794b105d54f15bca793f
Anton Lisitsyn <lisitsyn.anton.a@gmail.com>
2017-12-12 23:12:44 +0300

    added DropBoxMainScreen view controller

---
+++ dropoMusic/DropBoxMainScreenViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DropBoxMainScreenViewController.swift
+//  dropoMusic
+//
+//  Created by Anton on 12.12.17.
+//  Copyright © 2017 Anton. All rights reserved.
+//
+
+import UIKit
+
+class DropBoxMainScreenViewController: UITableViewController {
+
+    @IBOutlet weak var OverwriteExistingFilesSwitch: UISwitch!
+    @IBAction func logOut(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
--- dropoMusic/SourceViewController.swift
+++ dropoMusic/SourceViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 import SwiftyDropbox
 
 class SourceViewController: UIViewController {
-
+    
     @IBAction func logInButtonPressed(_ sender: UIButton) {
         DropboxClientsManager.authorizeFromController(UIApplication.shared, controller: self, openURL: {(url: URL) -> Void in UIApplication.shared.openURL(url)})
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if let client = DropboxClientsManager.authorizedClient {
+            
+            // List contents of app folder
+            _ = client.files.listFolder(path: "").response { response, error in
+                if let result = response {
+                    print("Folder contents:")
+                    for entry in result.entries {
+                        print(entry.name)
+                    }
+                }
+                // Do any additional setup after loading the view.
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+ 
     }
 }
